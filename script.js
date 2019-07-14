85330ee8298576c9f17e2df84d8a93362162c98c
Kamil Polanski <polanski88@gmail.com>
2019-07-14 16:22:15 +0200

    End of task

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 class App extends React.Component {
     constructor() {
-      super();
-      this.state = {
-        searchText: '',
-        users: []
-      };
+        super();
+        this.state = {
+            searchText: '',
+            users: []
+        };
     }
   
     onChangeHandle(event) {
-      this.setState({searchText: event.target.value});
+        this.setState({searchText: event.target.value});
     }
   
     onSubmit(event) {
-      event.preventDefault();
-      const {searchText} = this.state;
-      const url = `https://api.github.com/search/users?q=${searchText}`;
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => this.setState({users: responseJson.items}));
+        event.preventDefault();
+        const {searchText} = this.state;
+        const url = `https://api.github.com/search/users?q=${searchText}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(responseJson => this.setState({users: responseJson.items}));
     }
   
     render() {
-      return (
-        <div>
-          <form onSubmit={event => this.onSubmit(event)}>
-            <label htmlFor="searchText">Search by user name</label>
-            <input
-              type="text"
-              id="searchText"
-              onChange={event => this.onChangeHandle(event)}
-              value={this.state.searchText}/>
-          </form>
-          <UsersList users={this.state.users}/>
-        </div>
-      );
+        return (
+            <div>
+                <form className="form" onSubmit={event => this.onSubmit(event)}>
+                    <label htmlFor="searchText">Search by user name</label>
+                    <input
+                        type="text"
+                        id="searchText"
+                        onChange={event => this.onChangeHandle(event)}
+                        value={this.state.searchText}/>
+                </form>
+                <UsersList users={this.state.users}/>
+            </div>
+        );
     }
   }
 
   class UsersList extends React.Component {
     get users() {
-      return this.props.users.map(user => <User key={user.id} user={user}/>);
+        return this.props.users.map(user => <User key={user.id} user={user}/>);
     }
   
     render() {
-      return (
-        <div>
-          {this.users}
+        return (
+        <div className="user">
+            {this.users}
         </div>
       );
     }
@@ -55,8 +55,8 @@ class App extends React.Component {
     render() {
       return (
         <div>
-          <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
-          <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
+            <img src={this.props.user.avatar_url} style={{maxWidth: '100px'}}/>
+            <a href={this.props.user.html_url} target="_blank">{this.props.user.login}</a>
         </div>
       );
     }
